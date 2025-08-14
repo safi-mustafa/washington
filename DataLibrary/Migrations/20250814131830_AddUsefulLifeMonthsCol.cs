ae7338c5cc1edc6b9f6088beef85289d0f888362
Vikram <tbs.vikram@gmail.com>
2025-08-14 18:53:40 +0530

    add UsefulLifeMonths columns

---
+++ DataLibrary/Migrations/20250814131830_AddUsefulLifeMonthsCol.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsefulLifeMonthsCol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UsefulLifeMonths",
+                table: "Equipments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsefulLifeMonths",
+                table: "Equipments");
+        }
+    }
+}
