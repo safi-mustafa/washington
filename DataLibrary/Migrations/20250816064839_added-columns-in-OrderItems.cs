a56765bba43fe12dd3821b4c71dab1ccbb38d945
Bhavik Shah <bhavik.mscit@gmail.com>
2025-08-16 12:20:13 +0530

    Add StartDate and EndDate columns to OrderItems
    - Created a new migration to add nullable DateTime columns StartDate and EndDate to the OrderItems table.
    - Updated the ApplicationDbContextModelSnapshot to reflect the new columns.
    - Modified the OrderItems model to include StartDate and EndDate properties.

---
+++ DataLibrary/Migrations/20250816064839_added-columns-in-OrderItems.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedcolumnsinOrderItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "OrderItems");
+        }
+    }
+}
