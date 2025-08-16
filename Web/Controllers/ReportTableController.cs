9ccc2465c452d4ecdd4c6b8fbf2bcffabc898b2b
bhavikmit <bhavik.mscit@gmail.com>
2025-08-16 15:16:46 +0530

    Commit for client feedback changes for report, setting page

---
+++ Web/Controllers/ReportTableController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewModels.CRUD;
+using System.Collections.Generic;
+using System;
+
+namespace Web.Controllers
+{
+    public class ReportTableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
