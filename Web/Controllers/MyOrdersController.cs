2180fbd89dbb02ac596f0106f618c5db4393807c
Vikram <tbs.vikram@gmail.com>
2025-08-16 16:46:28 +0530

    submit change sof my order

--- Web/Controllers/MyOrdersController.cs
+++ Web/Controllers/MyOrdersController.cs
@@ -1,15 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ViewModels.CRUD;
-using System.Collections.Generic;
+
+using Repositories.Services.CustomerProfile.Interface;
+using Repositories.Services.MyOrder.Interface;
+
 using System;
+using System.Collections.Generic;
+
+using ViewModels.CRUD;
+using ViewModels.Shared.Notes;
 
 namespace Web.Controllers
 {
     public class MyOrdersController : Controller
     {
+        private readonly IMyOrderService _myOrderService;
+        private readonly ILogger<MyOrdersController> _logger;
+
+        public MyOrdersController(IMyOrderService myOrderService, ILogger<MyOrdersController> logger)
+        {
+            _myOrderService = myOrderService;
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> AllOrders()
+        {
+
+            try
+            {
+                var allOrders = await _myOrderService.GetAllOrders();
+                return PartialView("_AllOrder", allOrders);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"All Order threw an exception, Message: {ex.Message}"); return RedirectToAction("Index");
+            }
         }
     }
 }
